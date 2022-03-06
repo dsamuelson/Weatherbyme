e19f281e35486b75cbe8085842911896ef0063dc
David Samuelson <dsamuelson89@gmail.com>
2022-03-05 22:43:04 -0700

    fixed api calls and added gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,67 +79,60 @@ let getLocationData = function(cInfo) {
      //reverse the search to get the true name of the city from the coordinates
      let trueCityNameUrl = "https://api.openweathermap.org/geo/1.0/reverse?lat=" + cInfo[0].lat + "&lon=" + cInfo[0].lon + "&limit=1&appid=00ac63cafa4c25e7c142d8cdb9b63fc2";
      //get weather data
-     Promise.all([
-         fetch(apiUrl),
-         fetch(trueCityNameUrl)
-     ]).then(allResponses => {
-         const weatherInfo = allResponses[0].json()
-         const trueCity = allResponses[1].json()
-         displayWeatherInfo(weatherInfo, trueCity);
-     });
+     Promise.allSettled([
+         fetch(apiUrl).then(response => response.json()),
+         fetch(trueCityNameUrl).then(response => response.json())
+     ]).then(allResponses => displayWeatherInfo(allResponses[0].value, allResponses[1].value[0]));
 };
 
 let displayWeatherInfo = function(wInfo, tCityName){
     // clear current weather elements
-
+    console.log(wInfo,tCityName);
     cityCurrWeatherEl.innerHTML = "";
     cityFutWeatherEl.innerHTML = "";
 
     // get true city name from geolocation
 
-    tCityName.then(function(data) {
         let currCityName = document.createElement("h2");
-        currCityName.textContent = data[0].name + ", " + data[0].state + " (" + data[0].country + ") | " + currDate.toDateString();
+        currCityName.textContent = tCityName.name + ", " + tCityName.state + " (" + tCityName.country + ") | " + currDate.toDateString();
         let currWeatherIconEl = document.createElement("img");
         currWeatherIconEl.className = "weather-icon";
         currCityName.appendChild(currWeatherIconEl);
         cityCurrWeatherEl.appendChild(currCityName);
-    });
-    wInfo.then(function(data) {
+    
         //Create elements that will contain and show the information gathered from the data
 
-        let currWeatherIconEl = document.querySelector(".weather-icon");
-        currWeatherIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
-        currWeatherIconEl.setAttribute("alt", data.current.weather[0].description);
+        currWeatherIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + wInfo.current.weather[0].icon + "@2x.png");
+        currWeatherIconEl.setAttribute("alt", wInfo.current.weather[0].description);
 
         let currTempEl = document.createElement("p");
-        currTempEl.textContent = "Current Temperature: " + Math.floor((data.current.temp - 273.15) * (9 / 5) + 32) + " (F)/" + Math.floor((data.current.temp - 273.15)) + " (C) (or " + data.current.temp + " (K))";
+        currTempEl.textContent = "Current Temperature: " + Math.floor((wInfo.current.temp - 273.15) * (9 / 5) + 32) + " (F)/" + Math.floor((wInfo.current.temp - 273.15)) + " (C) (or " + wInfo.current.temp + " (K))";
         cityCurrWeatherEl.appendChild(currTempEl)
 
         let currWindEl = document.createElement("p");
-        currWindEl.textContent = "Wind Speed: " + (data.current.wind_speed * 2.237).toFixed(2) + " MPH";
+        currWindEl.textContent = "Wind Speed: " + (wInfo.current.wind_speed * 2.237).toFixed(2) + " MPH";
         cityCurrWeatherEl.appendChild(currWindEl)
 
         let currHumidityEl = document.createElement("p");
-        currHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
+        currHumidityEl.textContent = "Humidity: " + wInfo.current.humidity + "%";
         cityCurrWeatherEl.appendChild(currHumidityEl)
 
         let currUVIndexEl = document.createElement("p");
         let currUVDangerEl = document.createElement("span");
-        currUVDangerEl.textContent = data.current.uvi;
+        currUVDangerEl.textContent = wInfo.current.uvi;
         currUVDangerEl.id = "uv-danger";
 
         //set up color identifiers for UV index danger levels
 
-        if (data.current.uvi < 3) {
+        if (wInfo.current.uvi < 3) {
             currUVDangerEl.className = "uv-danger uv-low";
-        } else if (data.current.uvi >= 3 && data.current.uvi < 6 ) {
+        } else if (wInfo.current.uvi >= 3 && wInfo.current.uvi < 6 ) {
             currUVDangerEl.className = "uv-danger uv-moderate";
-        } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
+        } else if (wInfo.current.uvi >= 6 && wInfo.current.uvi < 8) {
             currUVDangerEl.className = "uv-danger uv-high";
-        } else if (data.current.uvi >= 8 && data.current.uvi < 11) {
+        } else if (wInfo.current.uvi >= 8 && wInfo.current.uvi < 11) {
             currUVDangerEl.className = "uv-danger uv-very-high";
-        } else if (data.current.uvi >= 11) {
+        } else if (wInfo.current.uvi >= 11) {
             currUVDangerEl.className = "uv-danger uv-severe";
         } else {
             currUVDangerEl.className = "uv-danger uv-noneFound";
@@ -152,38 +145,34 @@ let displayWeatherInfo = function(wInfo, tCityName){
         cityCurrWeatherEl.appendChild(currUVIndexEl)
 
         // create forecast boxes
-
         for (let i = 0 ; i < 5 ; i++){
-            let forcastData = data.daily[i];
+            let forcastData = wInfo.daily[i];
             let fBoxContEl = document.createElement("div");
             fBoxContEl.className = "f-box";
-
+    
             let fBoxDateEl = document.createElement("h4");
-            fBoxDateEl.textContent = forecastDay(currDate, (i + 1));
+            fBoxDateEl.textContent = new Date(forcastData.dt * 1000).toDateString();
             fBoxContEl.appendChild(fBoxDateEl);
-
+    
             let fBoxSymEl = document.createElement("img");
             fBoxSymEl.setAttribute("src", "https://openweathermap.org/img/wn/" + forcastData.weather[0].icon + ".png");
             fBoxSymEl.setAttribute("alt", forcastData.weather[0].description);
             fBoxContEl.appendChild(fBoxSymEl);
-
+    
             let fBoxTempEl = document.createElement("p");
             fBoxTempEl.textContent = "Temp: " + Math.floor((forcastData.temp.day - 273.15) * (9 / 5) + 32) + " (F)/" + Math.floor((forcastData.temp.day - 273.15)) + " (C)";
             fBoxContEl.appendChild(fBoxTempEl);
-
+    
             let fBoxWindEl = document.createElement("p");
             fBoxWindEl.textContent = "Wind: " + (forcastData.wind_speed * 2.237).toFixed(2) + " MPH";
             fBoxContEl.appendChild(fBoxWindEl);
-
+    
             let fBoxHumEl = document.createElement("p");
             fBoxHumEl.textContent = "Humidity: " + forcastData.humidity + "%";
             fBoxContEl.appendChild(fBoxHumEl);
-
+    
             cityFutWeatherEl.appendChild(fBoxContEl);
         }
-
-        
-    });
 
 };
 
